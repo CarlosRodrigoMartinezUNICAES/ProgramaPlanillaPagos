1f68a5faf46236ab8a899b52bcd64d6280220698
Raúl Medina <raul.medina@catolica.edu.sv>
2024-04-15 19:29:31 -0600

    Restablecer tracking de clases

---
+++ DatabaseConnection.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+public class DatabaseConnection
+{
+    private static string connectionString = @"Data Source=DESKTOP-8KSDFKL\SQLEXPRESS;Initial Catalog=Planilla;Integrated Security=True";
+    private static SqlConnection Connection;
+
+    private DatabaseConnection()
+    {
+        // Constructor privado para evitar instanciación directa
+    }
+
+    public static SqlConnection GetConnection()
+    {
+        if (Connection == null)
+        {
+            Connection = new SqlConnection(connectionString);
+        }
+        else if (Connection.State == System.Data.ConnectionState.Closed)
+        {
+            Connection.Open();
+        }
+
+        return Connection;
+    }
+
+    public static void CloseConnection()
+    {
+        if (Connection != null && Connection.State == System.Data.ConnectionState.Open)
+        {
+            Connection.Close();
+            //Connection = null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace ProgramaPlanillaPagos
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Empleados());
+        }
+    }
+}
